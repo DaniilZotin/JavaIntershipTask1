f5d115ce74f01002c6422da7fc9c1ce600c399d3
Daniil Zotin <you@gmail.com>
2022-12-28 15:50:20 +0200

    add new task

---
+++ src/main/java/org/example/Ex1/SortList.java
@@ -0,0 +1,22 @@
+package org.example.Ex1;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class SortList {
+    public String Sorting(List<Integer> list)
+    {
+        Iterator itr = list.iterator();
+        while(itr.hasNext())
+        {
+            int x = (int)itr.next();
+            if(x <= 0)
+            {
+                itr.remove();
+            }
+        }
+        System.out.println(list.toString());
+
+        return list.toString();
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.Ex1.SortList;
 import org.example.Ex2.Task_maps;
 import org.example.Ex3.SortAll;
 
@@ -10,14 +11,20 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
+        List <Integer> list = new ArrayList<Integer>(){{add(1);add(0);add(-1);add(3);}};
+        SortList sortList = new SortList();
+        sortList.Sorting(list);
+
         ArrayList<String> Text = new ArrayList<>();
         Text.add("#Hello wdadwa #wda #Hello #Helflo #Helfdalo");
         Text.add("awdgr #Heldwalo wdadwa #wda #Helflo");
         Text.add("#Helflo wdadwa #wwadda wdaadw #Hello");
         Task_maps taskMaps = new Task_maps();
         taskMaps.NewFunc(Text);
+
         List<String> form = List.of("Cube","Culindr","Bullet");
         SortAll sortAll = new SortAll();
         sortAll.getresult(form);
+
     }
 }
